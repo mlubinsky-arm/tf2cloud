926213114f17d846fc3e0f94ec6c3f247f15f222
mlubinsky-arm <michael.lubinsky@arm.com>
2020-04-14 19:20:11 -0700

    added tflite dependency

--- models/model.cpp
+++ models/model.cpp
@@ -1,18 +1,7 @@
 #include "model.h"
-// #include "models/data.hpp" // uTensor
-
+#include "tf/main_functions.h"
 // run inference
 float get_model_result(float* temp_value) {
+    setup();
     return 0.0f;
-/*
-    Context ctx;
-    Tensor* temp_value_tensor = new WrappedRamTensor<float>({1,10}, (float*) &temp_value);
-    get_data_ctx(ctx, temp_value_tensor);
-    printf("...Running Eval...");
-    ctx.eval();
-    printf("finished....");
-    S_TENSOR prediction = ctx.get({"dense_3_1/BiasAdd:0"});
-    float result = *(prediction->read<float>(0,0));
-    return result;
-*/
 }
